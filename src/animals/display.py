ce5efae4808d46dd2b25be9c3d993df141238968
Malu <maluandrade0307@gmail.com>
2025-05-21 20:48:17 -0300

    update animals display and manager

--- src/animals/display.py
+++ src/animals/display.py
@@ -19,12 +19,61 @@ def show_menu_animals():
               break
             case 1:
               #pesquisar
+              show_menu_animals()
             case 2:
               #adicionar
+              show_menu_adicionar()
             case 3: 
               #atualizar
+              show_menu_atualizar_animais()
             case 4:
-              #deltar
+              #deletar
+              show_menu_atualizar_animais()
             case _:
               print("Opção inválida.Escolha entre (0 - 4)")
               input("Pressione Enter para continuar")
+
+def show_menu_pesquisa_animais():
+   while True:
+      option = int(input(
+"""--PESQUISAR ANIMAIS--
+0)Voltar
+1)
+2)
+3)
+4)
+"""
+     ))
+def show_menu_adicionar():
+   while True:
+      option = int(input(
+"""--ADICIONAR ANIMAIS--
+0)Voltar
+1)
+2)
+3)
+4)
+"""
+    ))
+def show_menu_atualizar_animais():
+   while True:
+      option = int(input(
+"""--ATUALIZAR ANIMAIS--
+0)Voltar
+1)
+2)
+3)
+4)
+"""
+      ))
+def show_menu_deletar_animais():
+   while True:
+      option = int(input(
+"""--DELETAR ANIMAIS--
+0)Voltar
+1)
+2)
+3)
+4)
+"""
+      ))
--- src/animals/manager.py
+++ src/animals/manager.py
@@ -1,3 +1,9 @@
 """ 
 * MANAGER ANIMALS DATA
 """
+def pesquisa_animais():
+    pass
+def adicionar_animais():
+    pass
+def atualizar_animais():
+    pass
