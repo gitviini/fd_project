d13e81cbb097272a0f35aec7e01d3aed87a6edd8
MarcTony0 <marcosantoniofraga0@gmail.com>
2025-06-03 15:11:31 -0300

    Versão Final do CRUD na branch Animais

--- src/animals/manager.py
+++ src/animals/manager.py
@@ -1,6 +1,3 @@
-""" 
-* MANAGER ANIMALS DATA
-"""
 import json
 import os
 
@@ -8,9 +5,13 @@ ARQUIVO_JSON = os.path.join(os.path.dirname(__file__), "../../jsons/animals.json
 
 def load_json():
     if os.path.exists(ARQUIVO_JSON):
-        with open(ARQUIVO_JSON, "r", encoding="utf-8") as arquivo:
-            return json.load(arquivo)
-    return {}
+        try:
+            with open(ARQUIVO_JSON, "r", encoding="utf-8") as arquivo:
+                return json.load(arquivo)
+        except json.JSONDecodeError:
+            print("Erro: o arquivo JSON está vazio ou corrompido. Retornando dicionário vazio.")
+            return {}
+    return{}
 
 def save_json(banco_dados):
     with open(ARQUIVO_JSON, "w", encoding="utf-8") as arquivo:
